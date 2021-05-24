e9fadd04575a41cee8cd272ce211919ce1ed5632
Arun <abclingam1@gmail.com>
2021-05-24 16:27:44 +0530

    'todoapp'

--- db.js
+++ db.js
@@ -2,8 +2,12 @@ const dotenv = require('dotenv')
 dotenv.config()
 const mongodb = require('mongodb')
 
+let port =process.env.PORT
+if(port==null || port==""){
+  port=3000
+}
 mongodb.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
     module.exports = client
     const app = require('./app')
-    app.listen(process.env.PORT)
+    app.listen(port)
   })
